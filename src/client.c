e6484f610a8e6c3aa6d0706adcd944e251cf6f1e
Matt Basile <matthewbasile2@gmail.com>
2019-06-21 11:21:13 -0400

    parsing url correctly

--- src/client.c
+++ src/client.c
@@ -34,16 +34,25 @@ urlinfo_t *parse_url(char *url)
 
   urlinfo_t *urlinfo = malloc(sizeof(urlinfo_t));
 
-  /*
-    We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
-  */
+    // We can parse the input URL by doing the following:
+    // 1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
+      char *slash, *colon;
+      slash = strchr(hostname, '/');
+      printf("String after |/| is - |%s|\n",  slash);
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
+      path = slash + 1;
+      printf("Our path is [%s]\n", path);
+    // 3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+      *slash = '\0';
+      printf("Our hostname is [%s]\n", hostname);
+    // 4. Use strchr to find the first colon in the URL.
+      colon = strchr(hostname, ':');
+      printf("String after |:| is - |%s|\n",  colon);
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+      port = colon + 1;
+      printf("Our port is [%s]\n", port);
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+      *colon ='\0';
 
   ///////////////////
   // IMPLEMENT ME! //
@@ -84,7 +93,7 @@ int main(int argc, char *argv[])
     fprintf(stderr,"usage: client HOSTNAME:PORT/PATH\n");
     exit(1);
   }
-
+  parse_url(argv[1]);
   /*
     1. Parse the input URL
     2. Initialize a socket by calling the `get_socket` function from lib.c
